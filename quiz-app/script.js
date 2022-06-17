e2fc6d45673f83a3e58b76e5b165cc6a5a90e4ad
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-06-17 14:19:36 +0530

    Quiz App

---
+++ quiz-app/script.js
@@ -0,0 +1,43 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a:'10',
+        b:'17',
+        c:'26',
+        d:'110',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2020?',
+        a:'Java',
+        b:'C',
+        c:'Python',
+        d:'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'Who is the President of US?',
+        a:'Florin Pop',
+        b:'Donald Trump',
+        c:'Joe Buiden',
+        d:'Mihai Andrei',
+        correct: 'c'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a:'Hypertext Markup Language',
+        b:'Cascading Style Sheet',
+        c:'Jason momoa',
+        d:'Heli cop Tre MOto Lambo',
+        correct: 'a'
+    },
+    {
+        question: 'What year was JavaScript launched?',
+        a:'1996',
+        b:'1995',
+        c:'1994',
+        d:'None of the Above',
+        correct: 'd'
+    },
+
+]
